7c654a1622339b2324667f22206d1d35b1ad521f
AlienegraGeek <969024145@qq.com>
2023-11-15 14:58:56 +0800

    update

--- src/components/navContent.vue
+++ src/components/navContent.vue
@@ -18,7 +18,7 @@ const navSelectList = ['首页', '区块链', '合约', '统计', '资源']
 const selectIndex = ref(0)
 const address = ref('Connect Wallet');
 const chanegSelectIndex = (navIndex: number) => {
-    selectIndex.value = navIndex
+  selectIndex.value = navIndex
 }
 </script>
 <template>
@@ -38,7 +38,9 @@ const chanegSelectIndex = (navIndex: number) => {
         <el-dropdown @command="handleCommand">
           <el-button class="nav_select_right_title" @click="connectWallet">
             {{ address }}
-            <el-icon class="el-icon--right"><arrow-down /></el-icon>
+            <el-icon class="el-icon--right">
+              <arrow-down />
+            </el-icon>
           </el-button>
           <template #dropdown>
             <el-dropdown-menu>
@@ -67,137 +69,139 @@ const chanegSelectIndex = (navIndex: number) => {
           <div>Temporarily suspend the search of user for system updates</div>
         </div>
         <div class=""></div>
-    </div>
-    <div class="nav_content834" v-if="viewportWidth > 430 && viewportWidth <= 834">
+      </div>
+      <div class="nav_content834" v-if="viewportWidth > 430 && viewportWidth <= 834">
         <div></div>
-    </div>
-    <div class="nav_content430" v-if="viewportWidth <= 430">
+      </div>
+      <div class="nav_content430" v-if="viewportWidth <= 430">
         <div></div>
+      </div>
     </div>
+  </div>
 </template>
 <style scoped lang="scss">
 //大屏样式
 
 .nav_content1440 {
-    width: 100%;
-    height: 115px;
-
-    .nav_select {
-        display: flex;
+  width: 100%;
+  height: 115px;
+  
+  .nav_select {
+    display: flex;
+    height: 37px;
+    padding: 11px 32px;
+    justify-content: space-between;
+    
+    .nav_select_left {
+      display: flex;
+      align-items: center;
+      
+      img {
+        width: 32px;
         height: 37px;
-        padding: 11px 32px;
-        justify-content: space-between;
-
-        .nav_select_left {
-            display: flex;
-            align-items: center;
-
-            img {
-                width: 32px;
-                height: 37px;
-            }
-
-            .nav_select_left_title {
-                margin: 0 15px;
-                font-size: 20px;
-                font-family: PingFang SC;
-                font-weight: 600;
-                color: #191919;
-            }
-
-            .select_list {
-                display: flex;
-                align-items: center;
-
-
-                .select_list_item {
-                    box-sizing: border-box;
-                    width: 74px;
-                    height: 37px;
-                    line-height: 37px;
-                    font-size: 16px;
-                    font-family: PingFang SC;
-                    font-weight: 400;
-                    color: #191919;
-                    text-align: center;
-                    // border-bottom: 2px solid transparent;
-
-                    &:hover {
-                        cursor: pointer;
-                        color: #0FACB6;
-                    }
-                }
-
-                .active {
-                    color: #0FACB6;
-                    font-weight: 600;
-                    border-bottom: 2px solid #0FACB6;
-                }
-            }
+      }
+      
+      .nav_select_left_title {
+        margin: 0 15px;
+        font-size: 20px;
+        font-family: PingFang SC;
+        font-weight: 600;
+        color: #191919;
+      }
+      
+      .select_list {
+        display: flex;
+        align-items: center;
+        
+        
+        .select_list_item {
+          box-sizing: border-box;
+          width: 74px;
+          height: 37px;
+          line-height: 37px;
+          font-size: 16px;
+          font-family: PingFang SC;
+          font-weight: 400;
+          color: #191919;
+          text-align: center;
+          // border-bottom: 2px solid transparent;
+          
+          &:hover {
+            cursor: pointer;
+            color: #0FACB6;
+          }
         }
-
-        .nav_select_right {
-            display: flex;
-            align-items: center;
-
-            .nav_select_right_title {
-                width: 126px;
-                height: 29px;
-                line-height: 29px;
-                text-align: center;
-                color: #191919;
-                font-family: PingFang SC;
-                font-size: 12px;
-                font-style: normal;
-                font-weight: 400;
-                background-color: #3EDFCF;
-                border-radius: 76px;
-                margin-right: 10px;
-            }
-
-            .wallet_address_section {
-                width: 144px;
-                height: 29px;
-                background-color: #FFF;
-                border-radius: 77px;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                color: #191919;
-                font-family: PingFang SC;
-                font-size: 12px;
-                font-style: normal;
-                font-weight: 400;
-                margin-right: 12px;
-
-                img {
-                    width: 20px;
-                    height: 24px;
-                    margin-right: 9px;
-                }
-            }
-
-            .language_title {
-                color: #0FACB6;
-                font-family: PingFang SC;
-                font-size: 14px;
-                font-style: normal;
-                font-weight: 400;
-                margin-right: 6px;
-            }
-
-            .language_icon {
-                width: 12px;
-                height: 7.5px;
-            }
+        
+        .active {
+          color: #0FACB6;
+          font-weight: 600;
+          border-bottom: 2px solid #0FACB6;
         }
+      }
     }
-
-    // div {
-    //     width: 100px;
-    //     height: 115px;
-    //     background-color: red;
-    // }
+    
+    .nav_select_right {
+      display: flex;
+      align-items: center;
+      
+      .nav_select_right_title {
+        width: 126px;
+        height: 29px;
+        line-height: 29px;
+        text-align: center;
+        color: #191919;
+        font-family: PingFang SC;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        background-color: #3EDFCF;
+        border-radius: 76px;
+        margin-right: 10px;
+      }
+      
+      .wallet_address_section {
+        width: 144px;
+        height: 29px;
+        background-color: #FFF;
+        border-radius: 77px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #191919;
+        font-family: PingFang SC;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        margin-right: 12px;
+        
+        img {
+          width: 20px;
+          height: 24px;
+          margin-right: 9px;
+        }
+      }
+      
+      .language_title {
+        color: #0FACB6;
+        font-family: PingFang SC;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        margin-right: 6px;
+      }
+      
+      .language_icon {
+        width: 12px;
+        height: 7.5px;
+      }
+    }
+  }
+  
+  // div {
+  //     width: 100px;
+  //     height: 115px;
+  //     background-color: red;
+  // }
 }
 
 //中屏样式
@@ -217,6 +221,7 @@ const chanegSelectIndex = (navIndex: number) => {
 .example-showcase .el-dropdown + .el-dropdown {
   margin-left: 15px;
 }
+
 .example-showcase .el-dropdown-link {
   cursor: pointer;
   color: var(--el-color-primary);

f469cd96861d509b05ef792855f2b051bd808740
2568256629 <2568256629@qq.com>
2023-11-16 20:13:45 +0800

    添加区块链、合约、统计、资源文件，添加区块链页面内容

---
+++ src/page/home/contract.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// defineProps<{ msg: string }>()
+// const count = ref(0)
+import { onMounted } from 'vue';
+const windowWidth = ref(document.documentElement.clientWidth);
+//监听窗口大小变化
+onMounted(() => {
+    window.addEventListener('resize', () => {
+        windowWidth.value = document.documentElement.clientWidth
+        console.log(windowWidth.value);
+    });
+
+});
+</script>
+<template>
+    <div class="content">
+    </div>
+</template>
+<style scoped lang="scss">
+.content {
+    width: 100%;
+    min-height: 100vh;
+    background: #F2F0EA;
+}
+</style>
--- src/route/route.ts
+++ src/route/route.ts
@@ -4,6 +4,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../page/home/index.vue"; // 你的视图组件路径
 import DefaultHome from "../page/index.vue"; // 你的视图组件路径
 import Test from "../page/home/test.vue"; // 你的视图组件路径
+import BlockChain from "../page/home/blockChain.vue"; // 你的视图组件路径
+import Contract from "../page/home/contract.vue"; // 你的视图组件路径
+import Statistics from "../page/home/statistics.vue"; // 你的视图组件路径
+import Resource from "../page/home/resource.vue"; // 你的视图组件路径
 
 const routes = [
   {
@@ -18,6 +22,22 @@ const routes = [
       {
         path: "/test",
         component: Test,
+      },
+      {
+        path: "/blockchain",
+        component: BlockChain,
+      },
+      {
+        path: "/contract",
+        component: Contract,
+      },
+      {
+        path: "/statistics",
+        component: Statistics,
+      },
+      {
+        path: "/resource",
+        component: Resource,
       },
     ],
   },

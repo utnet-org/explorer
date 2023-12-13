69e3541ee799b76e0ec73ed0a86ae39905f4c155
2568256629 <2568256629@qq.com>
2023-12-13 17:54:26 +0800

    run format

--- src/components/footerContent.vue
+++ src/components/footerContent.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-
-const routeLink = (url: string) => {
-  window.open(url);
-}
+  const routeLink = (url: string) => {
+    window.open(url);
+  };
 </script>
 <template>
   <div class="footer_content">
@@ -23,7 +22,10 @@ const routeLink = (url: string) => {
           <p>{{ $t('home.contact_us') }}</p>
         </div>
         <div class="black-text-12">
-          <div class="contact-item" @click="routeLink('https://x.com/UtilityNet_')">
+          <div
+            class="contact-item"
+            @click="routeLink('https://x.com/UtilityNet_')"
+          >
             <img src="../assets/images/twitter.png" alt="" />
             <p style="margin: 0">{{ $t('home.twitter') }}</p>
           </div>
@@ -31,7 +33,12 @@ const routeLink = (url: string) => {
             <img src="../assets/images/telegram.png" alt="" />
             <p>{{ $t('home.telegram') }}</p>
           </div>
-          <div class="contact-item" @click="routeLink('https://www.google.com/intl/zh-CN_cn/gmail/about/')">
+          <div
+            class="contact-item"
+            @click="
+              routeLink('https://www.google.com/intl/zh-CN_cn/gmail/about/')
+            "
+          >
             <img src="../assets/images/email.png" alt="" />
             <p style="margin: 0">{{ $t('home.mail') }}</p>
           </div>
@@ -42,9 +49,18 @@ const routeLink = (url: string) => {
           <p>{{ $t('home.community') }}</p>
         </div>
         <div class="black-text-12">
-          <p @click="routeLink('http://oss2.xuanwoo.com/UtilityNetWhitePaper.pdf')">{{ $t('home.white_book') }}</p>
-          <p @click="routeLink('https://wiki.utlab.io/')">{{ $t('home.api_doc') }}</p>
-          <p @click="routeLink('https://wiki.utlab.io/')">{{ $t('home.dev_guide') }}</p>
+          <p
+            @click="
+              routeLink('http://oss2.xuanwoo.com/UtilityNetWhitePaper.pdf')
+            "
+            >{{ $t('home.white_book') }}</p
+          >
+          <p @click="routeLink('https://wiki.utlab.io/')">{{
+            $t('home.api_doc')
+          }}</p>
+          <p @click="routeLink('https://wiki.utlab.io/')">{{
+            $t('home.dev_guide')
+          }}</p>
         </div>
       </div>
       <div>
@@ -55,7 +71,9 @@ const routeLink = (url: string) => {
           <p @click="routeLink('https://utlab.io')">{{ $t('home.website') }}</p>
           <p>{{ $t('home.app_mobile') }}</p>
           <p>{{ $t('home.explorer_wallet') }}</p>
-          <p @click="routeLink('https://faucet.utlab.io/')">{{ $t('home.faucet') }}</p>
+          <p @click="routeLink('https://faucet.utlab.io/')">{{
+            $t('home.faucet')
+          }}</p>
         </div>
       </div>
     </div>
@@ -65,207 +83,209 @@ const routeLink = (url: string) => {
   </div>
 </template>
 <style scoped lang="scss">
-@media screen {
-  .footer_content {
-    width: 100%;
-    background: var(--9-bded-6,
-        linear-gradient(180deg, rgba(155, 222, 214, 0.72) 0%, #9bded6 100%));
-    backdrop-filter: blur(5px);
-  }
+  @media screen {
+    .footer_content {
+      width: 100%;
+      background: var(
+        --9-bded-6,
+        linear-gradient(180deg, rgba(155, 222, 214, 0.72) 0%, #9bded6 100%)
+      );
+      backdrop-filter: blur(5px);
+    }
 
-  .footer-header {
-    height: 150px;
-    padding: 32px 32px;
-  }
+    .footer-header {
+      height: 150px;
+      padding: 32px 32px;
+    }
 
-  .icon_title {
-    padding: 0 0 10px 0;
-    display: flex;
-    align-items: center;
-
-    .bg {
-      width: 44px;
-      height: 44px;
-      background-color: #fff;
-      border-radius: 50%;
+    .icon_title {
+      padding: 0 0 10px 0;
       display: flex;
-      justify-content: center;
       align-items: center;
-      margin-right: 8px;
 
-      img {
-        width: 25px;
-        height: 29px;
+      .bg {
+        width: 44px;
+        height: 44px;
+        background-color: #fff;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-right: 8px;
+
+        img {
+          width: 25px;
+          height: 29px;
+        }
       }
     }
-  }
 
-  .link-content {
-    width: 65%;
-    max-width: 800px;
-    padding: 0 32px 20px 32px;
-    display: flex;
-    justify-content: space-between;
-    margin-left: auto;
-  }
+    .link-content {
+      width: 65%;
+      max-width: 800px;
+      padding: 0 32px 20px 32px;
+      display: flex;
+      justify-content: space-between;
+      margin-left: auto;
+    }
 
-  .contact-item {
-    display: flex;
-    align-items: center;
+    .contact-item {
+      display: flex;
+      align-items: center;
 
-    img {
-      margin-right: 5px;
+      img {
+        margin-right: 5px;
+      }
+    }
+
+    p:hover {
+      cursor: pointer;
+      //border-bottom: 1px solid #0facb6;
+      color: #0facb6;
+    }
+
+    .father_bottom {
+      height: 37px;
+      display: flex;
+      justify-content: end;
+      align-items: center;
+      border-top: 0.5px solid rgba(25, 25, 25, 0.3);
+      margin: 0 16px;
     }
   }
 
-  p:hover {
-    cursor: pointer;
-    //border-bottom: 1px solid #0facb6;
-    color: #0facb6;
-  }
-
-  .father_bottom {
-    height: 37px;
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    border-top: 0.5px solid rgba(25, 25, 25, 0.3);
-    margin: 0 16px;
-  }
-}
-
-//@media (min-width: 1024px) {
-//  .footer_content {
-//    width: 100%;
-//    background: var(
-//        --9-bded-6,
-//        linear-gradient(180deg, rgba(155, 222, 214, 0.72) 0%, #9bded6 100%)
-//    );
-//    backdrop-filter: blur(5px);
-//
-//    .footer_top {
-//      .top_left {
-//        .icon_title {
-//          padding-top: 20px;
-//          margin-bottom: 17px;
-//          margin-left: 32px;
-//          display: flex;
-//          align-items: center;
-//
-//          .bg {
-//            width: 55px;
-//            height: 55px;
-//            background-color: #fff;
-//            border-radius: 50%;
-//            display: flex;
-//            justify-content: center;
-//            align-items: center;
-//          }
-//
-//          img {
-//            width: 32px;
-//            height: 37px;
-//          }
-//
-//          .title {
-//            margin-left: 8px;
-//            font-size: 20px;
-//            font-weight: 600;
-//            color: #191919;
-//          }
-//        }
-//
-//        .text {
-//          margin-left: 32px;
-//          font-size: 14px;
-//          font-weight: 600;
-//          color: #191919;
-//        }
-//      }
-//
-//      .top_right {
-//        display: flex;
-//
-//        .right_left,
-//        .right_center,
-//        .right_right {
-//          display: flex;
-//          flex-direction: column;
-//
-//          div {
-//            margin-top: 4px;
-//            font-size: 12px;
-//            font-weight: 500;
-//            color: rgba(25, 25, 25, 0.8);
-//          }
-//        }
-//
-//        .right_center {
-//          margin: 0 70px;
-//        }
-//
-//        .right_right {
-//          span:hover {
-//            cursor: pointer; //鼠标移上去变成小手
-//            border-bottom: 1px solid #0facb6;
-//            color: #0facb6;
-//          }
-//        }
-//      }
-//    }
-//
-//    .footer_bottom {
-//      display: flex;
-//      justify-content: space-between;
-//      width: 100%;
-//      align-items: center;
-//
-//      .father_bottom {
-//        display: flex;
-//        justify-content: end;
-//        border-top: 0.5px solid rgba(25, 25, 25, 0.3);
-//        width: 100%;
-//        margin: 0 16px;
-//        height: 100%;
-//        align-items: center;
-//      }
-//
-//      .bottom_left {
-//        display: flex;
-//        margin-left: 9px;
-//
-//        img {
-//          width: 18px;
-//          height: 18px;
-//          margin-right: 14px;
-//        }
-//      }
-//
-//      .bottom_right {
-//        margin-right: 8px;
-//        font-size: 14px;
-//        font-weight: 400;
-//        color: rgba(25, 25, 25, 0.6);
-//      }
-//    }
-//  }
-//  .footer_content {
-//    height: 238px;
-//  }
-//  .footer_top {
-//    display: flex;
-//    justify-content: space-between;
-//    width: 100%;
-//    height: 197px;
-//  }
-//  .footer_bottom {
-//    height: 41px;
-//  }
-//  .top_right {
-//    margin-top: 78px;
-//  }
-//  .right_right {
-//    margin-right: 54px;
-//  }
-//}
+  //@media (min-width: 1024px) {
+  //  .footer_content {
+  //    width: 100%;
+  //    background: var(
+  //        --9-bded-6,
+  //        linear-gradient(180deg, rgba(155, 222, 214, 0.72) 0%, #9bded6 100%)
+  //    );
+  //    backdrop-filter: blur(5px);
+  //
+  //    .footer_top {
+  //      .top_left {
+  //        .icon_title {
+  //          padding-top: 20px;
+  //          margin-bottom: 17px;
+  //          margin-left: 32px;
+  //          display: flex;
+  //          align-items: center;
+  //
+  //          .bg {
+  //            width: 55px;
+  //            height: 55px;
+  //            background-color: #fff;
+  //            border-radius: 50%;
+  //            display: flex;
+  //            justify-content: center;
+  //            align-items: center;
+  //          }
+  //
+  //          img {
+  //            width: 32px;
+  //            height: 37px;
+  //          }
+  //
+  //          .title {
+  //            margin-left: 8px;
+  //            font-size: 20px;
+  //            font-weight: 600;
+  //            color: #191919;
+  //          }
+  //        }
+  //
+  //        .text {
+  //          margin-left: 32px;
+  //          font-size: 14px;
+  //          font-weight: 600;
+  //          color: #191919;
+  //        }
+  //      }
+  //
+  //      .top_right {
+  //        display: flex;
+  //
+  //        .right_left,
+  //        .right_center,
+  //        .right_right {
+  //          display: flex;
+  //          flex-direction: column;
+  //
+  //          div {
+  //            margin-top: 4px;
+  //            font-size: 12px;
+  //            font-weight: 500;
+  //            color: rgba(25, 25, 25, 0.8);
+  //          }
+  //        }
+  //
+  //        .right_center {
+  //          margin: 0 70px;
+  //        }
+  //
+  //        .right_right {
+  //          span:hover {
+  //            cursor: pointer; //鼠标移上去变成小手
+  //            border-bottom: 1px solid #0facb6;
+  //            color: #0facb6;
+  //          }
+  //        }
+  //      }
+  //    }
+  //
+  //    .footer_bottom {
+  //      display: flex;
+  //      justify-content: space-between;
+  //      width: 100%;
+  //      align-items: center;
+  //
+  //      .father_bottom {
+  //        display: flex;
+  //        justify-content: end;
+  //        border-top: 0.5px solid rgba(25, 25, 25, 0.3);
+  //        width: 100%;
+  //        margin: 0 16px;
+  //        height: 100%;
+  //        align-items: center;
+  //      }
+  //
+  //      .bottom_left {
+  //        display: flex;
+  //        margin-left: 9px;
+  //
+  //        img {
+  //          width: 18px;
+  //          height: 18px;
+  //          margin-right: 14px;
+  //        }
+  //      }
+  //
+  //      .bottom_right {
+  //        margin-right: 8px;
+  //        font-size: 14px;
+  //        font-weight: 400;
+  //        color: rgba(25, 25, 25, 0.6);
+  //      }
+  //    }
+  //  }
+  //  .footer_content {
+  //    height: 238px;
+  //  }
+  //  .footer_top {
+  //    display: flex;
+  //    justify-content: space-between;
+  //    width: 100%;
+  //    height: 197px;
+  //  }
+  //  .footer_bottom {
+  //    height: 41px;
+  //  }
+  //  .top_right {
+  //    margin-top: 78px;
+  //  }
+  //  .right_right {
+  //    margin-right: 54px;
+  //  }
+  //}
 </style>
--- src/page/resource/knowledgeBase.vue
+++ src/page/resource/knowledgeBase.vue
@@ -1,31 +1,29 @@
 <script setup lang="ts">
-// 最新区块
-import { onMounted, ref } from 'vue';
-import HeaderPage from '../../components/otherHeaderContent.vue';
-// const windowWidth = ref(document.documentElement.clientWidth);
+  // 最新区块
+  import { onMounted, ref } from 'vue';
+  import HeaderPage from '../../components/otherHeaderContent.vue';
+  // const windowWidth = ref(document.documentElement.clientWidth);
 
-//监听窗口大小变化
-onMounted(() => {
-
-});
-const articleList = [
-  {
-    id: '',
-    title: '入门篇'
-  },
-  {
-    id: '',
-    title: '挖矿篇'
-  },
-  {
-    id: '',
-    title: '开发篇',
-  }
-]
-const currentIndex = ref(0);
-const changeContent = (index: number) => {
-  currentIndex.value = index;
-}
+  //监听窗口大小变化
+  onMounted(() => {});
+  const articleList = [
+    {
+      id: '',
+      title: '入门篇',
+    },
+    {
+      id: '',
+      title: '挖矿篇',
+    },
+    {
+      id: '',
+      title: '开发篇',
+    },
+  ];
+  const currentIndex = ref(0);
+  const changeContent = (index: number) => {
+    currentIndex.value = index;
+  };
 </script>
 <template>
   <div class="content">
@@ -36,9 +34,13 @@ const changeContent = (index: number) => {
       <div class="block_list_side">
         <div class="block_list_side_header">文章列表</div>
         <div class="block_list_side_list">
-          <div class="block_list_side_list_item" v-for="(item, index) in articleList" :key="index"
-            @click="changeContent(index)">{{ index + 1 }}.{{
-              item.title }}</div>
+          <div
+            class="block_list_side_list_item"
+            v-for="(item, index) in articleList"
+            :key="index"
+            @click="changeContent(index)"
+            >{{ index + 1 }}.{{ item.title }}</div
+          >
         </div>
       </div>
       <div class="block_list_content" v-if="currentIndex == 0">
@@ -57,114 +59,120 @@ const changeContent = (index: number) => {
   </div>
 </template>
 <style scoped lang="scss">
-.content {
-  width: 100%;
-  min-height: 100vh;
-  background: #f2f0ea;
-  position: relative;
-
-  .block_list {
-    width: calc(100% - 64px);
+  .content {
+    width: 100%;
     min-height: 100vh;
-    border-radius: 8px;
+    background: #f2f0ea;
     position: relative;
-    z-index: 10;
-    margin: 39px auto 36px;
-    display: flex;
-    justify-content: center;
 
-    .block_list_side {
-      width: 20%;
+    .block_list {
+      width: calc(100% - 64px);
       min-height: 100vh;
       border-radius: 8px;
-      border: 1px solid #3EDFCF;
-      background: #F9F9F8;
-      box-shadow: 0px 4px 15px 0px rgba(92, 255, 243, 0.10), 0px 4px 8px 0px rgba(0, 0, 0, 0.04);
-      margin-right: 25px;
+      position: relative;
+      z-index: 10;
+      margin: 39px auto 36px;
+      display: flex;
+      justify-content: center;
 
-      .block_list_side_header {
-        height: 68px;
-        line-height: 68px;
+      .block_list_side {
+        width: 20%;
+        min-height: 100vh;
         border-radius: 8px;
-        background: #FFF;
-        box-shadow: 0px 4px 15px 0px rgba(92, 255, 243, 0.10), 0px 4px 8px 0px rgba(0, 0, 0, 0.04);
-        color: #0687A4;
-        font-family: PingFang SC;
-        font-size: 25px;
-        font-weight: 600;
-        padding-left: 22px;
+        border: 1px solid #3edfcf;
+        background: #f9f9f8;
+        box-shadow:
+          0px 4px 15px 0px rgba(92, 255, 243, 0.1),
+          0px 4px 8px 0px rgba(0, 0, 0, 0.04);
+        margin-right: 25px;
+
+        .block_list_side_header {
+          height: 68px;
+          line-height: 68px;
+          border-radius: 8px;
+          background: #fff;
+          box-shadow:
+            0px 4px 15px 0px rgba(92, 255, 243, 0.1),
+            0px 4px 8px 0px rgba(0, 0, 0, 0.04);
+          color: #0687a4;
+          font-family: PingFang SC;
+          font-size: 25px;
+          font-weight: 600;
+          padding-left: 22px;
+        }
+
+        .block_list_side_list {
+          display: flex;
+          flex-direction: column;
+          padding: 20px;
+
+          .block_list_side_list_item {
+            color: #000;
+            font-family: PingFang SC;
+            font-size: 20px;
+            font-weight: 400;
+            margin-bottom: 10px;
+            cursor: pointer;
+
+            &:hover {
+              color: #3edfcf;
+            }
+          }
+        }
       }
 
-      .block_list_side_list {
-        display: flex;
-        flex-direction: column;
-        padding: 20px;
+      .block_list_content {
+        width: calc(80% - 25px);
+        min-height: 100vh;
+        border-radius: 8px;
+        background: #f9f9f8;
+        box-shadow:
+          0px 4px 15px 0px rgba(92, 255, 243, 0.1),
+          0px 4px 8px 0px rgba(0, 0, 0, 0.04);
+        padding: 16px;
+        box-sizing: border-box;
+        color: #000;
+        font-family: PingFang SC;
+        font-size: 20px;
+        font-weight: 400;
 
-        .block_list_side_list_item {
-          color: #000;
-          font-family: PingFang SC;
-          font-size: 20px;
-          font-weight: 400;
-          margin-bottom: 10px;
-          cursor: pointer;
+        h2 {
+          margin: 0;
+          padding: 10px 0;
+          border-bottom: 1px solid #eaecef;
+        }
 
-          &:hover {
-            color: #3EDFCF;
+        h3 {
+          padding: 10px 0;
+          border-bottom: 1px solid #eaecef;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 320px) and (max-width: 843px) {
+    .content {
+      .block_list {
+        width: calc(100% - 36px);
+        margin: 0 18px 36px;
+        box-sizing: border-box;
+
+        .block_list_side {
+          display: none;
+        }
+
+        .block_list_content {
+          width: 100%;
+
+          h2 {
+            font-size: 22px;
+          }
+
+          h3 {
+            font-size: 18px;
           }
         }
       }
     }
-
-    .block_list_content {
-      width: calc(80% - 25px);
-      min-height: 100vh;
-      border-radius: 8px;
-      background: #F9F9F8;
-      box-shadow: 0px 4px 15px 0px rgba(92, 255, 243, 0.10), 0px 4px 8px 0px rgba(0, 0, 0, 0.04);
-      padding: 16px;
-      box-sizing: border-box;
-      color: #000;
-      font-family: PingFang SC;
-      font-size: 20px;
-      font-weight: 400;
-
-      h2 {
-        margin: 0;
-        padding: 10px 0;
-        border-bottom: 1px solid #eaecef;
-      }
-
-      h3 {
-        padding: 10px 0;
-        border-bottom: 1px solid #eaecef;
-      }
-    }
   }
-}
-
-@media (min-width: 320px) and (max-width: 843px) {
-  .content {
-    .block_list {
-      width: calc(100% - 36px);
-      margin: 0 18px 36px;
-      box-sizing: border-box;
-
-      .block_list_side {
-        display: none;
-      }
-
-      .block_list_content {
-        width: 100%;
-
-        h2 {
-          font-size: 22px;
-        }
-
-        h3 {
-          font-size: 18px;
-        }
-      }
-    }
-  }
-}
 </style>

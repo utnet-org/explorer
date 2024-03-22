e262ef09db20f14f025f3be788d5c6ce1aa027a0
AlienegraGeek <969024145@qq.com>
2024-03-22 14:28:28 +0800

    last block

--- src/api/block.ts
+++ src/api/block.ts
@@ -10,6 +10,20 @@ export interface BlockInfo {
   rewards?: number[]; // 奖励
 }
 
+export interface LastBlock {
+  height?: number; // 高度
+  hash?: string; // 交易Hash
+  chunkHash?: string; // chunkHash
+  timestamp?: number; // 时间
+  author?: string; // 矿工
+  gasPrice?: number; // Gas价格
+  gasLimit?: number; // Gas限制
+}
+
 export function getBlockInfo() {
   return axios.post<BlockInfo[]>('/api/block/info');
+}
+
+export function getLastBlock() {
+  return axios.post<LastBlock[]>('/api/block/last');
 }

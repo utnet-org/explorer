9850e28d5231eb2aeab574be4ce65684ad4a3d99
CRT-123 <1130894061@qq.com>
2024-05-15 11:59:36 +0800

    1

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -42,7 +42,7 @@ export default defineConfig({
         rewrite: path => path.replace(/^\/api\/mock/, ''), // 去掉代理路径中的/api/mock
       },
       '/api': {
-        target: 'http://0.0.0.0:1080', // 后端真实数据服务地址
+        target: 'http://192.168.10.37:1080', // 后端真实数据服务地址
         changeOrigin: true,
         // rewrite: path => path.replace(/^\/api/, ''), // 去掉代理路径中的/api
       },

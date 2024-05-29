104218abca86b861424577abdbeb1075be121173
AlienegraGeek <969024145@qq.com>
2024-05-29 16:50:41 +0800

    Wasm instantiate

--- src/components/WasmComponent.vue
+++ src/components/WasmComponent.vue
@@ -16,48 +16,58 @@
       const wasmBinary = Uint8Array.from(atob(wasmBase64.value), c =>
         c.charCodeAt(0),
       );
+      // test simple wasm
+      // const wasmBinary = await (await fetch('/simple.wasm')).arrayBuffer();
       const wasmModule = await WebAssembly.compile(wasmBinary);
-      // const imports = {
-      //   env: {
-      //     abort: () => console.log('Abort called'),
-      //     promise_results_count: () => 0,
-      //     promise_result: () => {},
-      //     storage_has_key: () => {
-      //       return 1;
-      //     },
-      //     storage_read: () => {},
-      //     storage_write: () => {},
-      //     storage_remove: () => {},
-      //     storage_iter_create: () => {},
-      //     storage_iter_next: () => {},
-      //     memory: new WebAssembly.Memory({ initial: 256, maximum: 512 }),
-      //     table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
-      //     promise_batch_then: () => {},
-      //     promise_batch_create: () => {},
-      //     promise_batch_action_create_account: () => {},
-      //     promise_batch_action_deploy_contract: () => {},
-      //     promise_batch_action_function_call: () => {},
-      //     promise_batch_action_function_call_weight: () => {},
-      //     promise_batch_action_transfer: () => {},
-      //     promise_batch_action_stake: () => {},
-      //     promise_batch_action_add_key_with_full_access: () => {},
-      //     promise_batch_action_add_key_with_function_call: () => {},
-      //     promise_batch_action_delete_key: () => {},
-      //     promise_batch_action_delete_account: () => {},
-      //     promise_and: () => {},
-      //     promise_return: () => {},
-      //     panic_utf8: () => {},
-      //     register_len: () => {},
-      //     read_register: () => {},
-      //   },
-      // };
-      wasmInstance = await WebAssembly.instantiate(wasmModule);
+      const imports = {
+        env: {
+          abort: () => {},
+          promise_results_count: () => {},
+          promise_result: () => {},
+          storage_has_key: () => {},
+          storage_read: () => {},
+          storage_write: () => {},
+          storage_remove: () => {},
+          storage_iter_create: () => {},
+          storage_iter_next: () => {},
+          memory: new WebAssembly.Memory({ initial: 256, maximum: 512 }),
+          table: new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
+          promise_batch_then: () => {},
+          promise_batch_create: () => {},
+          promise_batch_action_create_account: () => {},
+          promise_batch_action_deploy_contract: () => {},
+          promise_batch_action_function_call: () => {},
+          promise_batch_action_function_call_weight: () => {},
+          promise_batch_action_transfer: () => {},
+          promise_batch_action_stake: () => {},
+          promise_batch_action_add_key_with_full_access: () => {},
+          promise_batch_action_add_key_with_function_call: () => {},
+          promise_batch_action_delete_key: () => {},
+          promise_batch_action_delete_account: () => {},
+          promise_and: () => {},
+          promise_return: () => {},
+          panic_utf8: () => {},
+          register_len: () => {},
+          read_register: () => {},
+          current_account_id: () => {},
+          signer_account_pk: () => {},
+          predecessor_account_id: () => {},
+          input: () => {},
+          attached_deposit: () => {},
+          value_return: () => {},
+          log_utf8: () => {},
+        },
+      };
+      wasmInstance = await WebAssembly.instantiate(wasmModule, imports);
       functions.value = [];
-      for (const name in wasmInstance.exports) {
-        if (typeof wasmInstance.exports[name] === 'function') {
+      const ex = wasmInstance.exports;
+      for (const name in ex) {
+        debugger;
+        if (typeof ex[name] === 'function') {
           functions.value.push(name);
         }
       }
+      console.log(functions.value);
     } catch (err) {
       console.error('Error loading Wasm:', err);
     }
